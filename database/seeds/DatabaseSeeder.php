28edad2c21031411dd9efd19b77f5b74379e4b04
YuriiShkinder <yurkaaa96@gmail.com>
2018-04-25 20:30:23 +0300

    ggggvffnncc

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -43,12 +43,12 @@ class DatabaseSeeder extends Seeder
 
     public function getCatArt(){
         $this->truncate('categories_articles');
-        $articles=\App\Article::all();
-        $cat=\App\Category::all()->count();
+        $articles=\App\Article::all()->random()->id;
+        $cat=\App\Category::all()->random()->id;
         foreach ($articles as $article){
             DB::table('categories_articles')->insert([
-                'category_id'=>rand(1,$cat),
-                'article_id'=>$article->id
+                'category_id'=>$cat,
+                'article_id'=>$articles
             ]);
         }
 
