c5f31a3d7192cb7646b8438894ff1b84155fddd3
YuriiShkinder <yurkaaa96@gmail.com>
2018-04-25 17:30:56 +0300

    change comments

--- database/migrations/2018_04_24_173323_change_comment_table.php
+++ database/migrations/2018_04_24_173323_change_comment_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeCommentTable extends Migration
+class ChangesCommentTable extends Migration
 {
     /**
      * Run the migrations.
